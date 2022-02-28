b4a394c79f8f6b1fa2dc72848de4e3188de704eb
Hamza Haddani <hhamza@student.1337.ma>
2022-02-28 08:16:11 +0100

    Add stack B protection
    Protect stack B allocation in main function

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hhamza <hhamza@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 12:19:48 by hhamza            #+#    #+#             */
-/*   Updated: 2022/02/23 09:49:20 by hhamza           ###   ########.fr       */
+/*   Updated: 2022/02/28 09:28:34 by hhamza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,11 @@ int	main(int argc, char **argv)
 		return (EXIT_FAILURE);
 	}
 	stack_b = ft_stack_new();
+	if (stack_b == NULL)
+	{
+		ft_stack_clear(&stack_a);
+		return (EXIT_FAILURE);
+	}
 	if (ft_sort(stack_a, stack_b) == FALSE)
 		return (EXIT_FAILURE);
 	ft_stack_clear(&stack_a);
